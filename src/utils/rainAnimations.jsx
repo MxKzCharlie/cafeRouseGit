a07b9a8d8e855e1e55a6789bc2b06813f3263102
CafeRouse21 <caferouseweb@gmail.com>
2024-10-31 16:51:59 -0700

    rainAnimation config height

--- src/utils/rainAnimations.jsx
+++ src/utils/rainAnimations.jsx
@@ -1,6 +1,6 @@
 function RainAnimations({img}) {
     return ( 
-        <div className="w-[22rem] h-32 overflow-hidden cursor-pointer text-4xl flex justify-center items-center
+        <div className="w-[22rem] h-full overflow-hidden cursor-pointer text-4xl flex justify-center items-center
         group absolute">
             <img src={img} 
             className="w-auto h-auto  transform -translate-y-[43rem]
