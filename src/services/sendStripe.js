49551e2f0a2d52a830c881dfacc10eccf8e8e4bf
CafeRouse21 <caferouseweb@gmail.com>
2024-11-01 09:46:40 -0700

    last commit stripe test mode

--- src/services/sendStripe.js
+++ src/services/sendStripe.js
@@ -1,6 +1,6 @@
 export const sendStripe = async (total) => {
     try {
-        const response = await fetch('https://cafe-rouse-backend.vercel.app/api/stripe', {
+        const response = await fetch('https://cafe-rouse-backend.vercel.app/api/stripe/create-checkout-session', {
             method: 'POST',
             headers: {
              'Content-Type': 'application/json',
