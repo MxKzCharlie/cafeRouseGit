1de8cb0c2bc5aa87f32773658334f5dcc2a84e95
CafeRouse21 <caferouseweb@gmail.com>
2024-11-01 09:41:21 -0700

    fix name of function in sendStripe

--- src/services/sendStripe.js
+++ src/services/sendStripe.js
@@ -1,4 +1,4 @@
-export const sendOrderDelivery = async (total) => {
+export const sendStripe = async (total) => {
     try {
         const response = await fetch('https://cafe-rouse-backend.vercel.app/api/stripe', {
             method: 'POST',
