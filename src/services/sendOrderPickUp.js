3e77216ca0909a3ad9f2edfdb33047b661e8f604
CafeRouse21 <caferouseweb@gmail.com>
2024-11-01 14:02:08 -0700

    fix finish order

--- src/services/sendOrderPickUp.js
+++ src/services/sendOrderPickUp.js
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom"
+
 export const sendOrderPickUp = async (dataClient, order) => {
+    const navigate = useNavigate();
+
     try {
         const response = await fetch('https://cafe-rouse-backend.vercel.app/api/twilio/send-sms-pickup', {
             method: 'POST',
@@ -13,7 +17,8 @@ export const sendOrderPickUp = async (dataClient, order) => {
 
         const result = await response.json();
         if(result.success){
-            alert('Mensaje enviado con exito');
+            alert('Mensaje enviado con exito, sera redirigido al inicio');
+            navigate("/");
         }else {
             alert('No se envio el mensaje, intentelo mas tarde');
             console.log('Error al enviar el mensaje');
