98d1f8d26efea1818742dd980b97786a006be579
CafeRouse21 <caferouseweb@gmail.com>
2024-11-01 21:53:42 -0700

    fix details delivery finish try

--- src/components/thankyouPage/delivery.jsx
+++ src/components/thankyouPage/delivery.jsx
@@ -7,15 +7,15 @@ function Delivery() {
     const {dataClient, setDataClient} = useContext(DataClientContext);
     const {count, setCount} = useContext(CarContext);
     
-    useEffect(async () => {
-        sendOrderDelivery(dataClient, count);
+    useEffect( async () => {
+        await sendOrderDelivery(dataClient, count);
     }, []);
     
     return (  
         <div className="h-auto w-full flex flex-col justify-center items-center gap-4 my-28">
             <h1 className="text-center text-8xl font-titles">¡Gracias Por Su Compra!</h1>
             <span className="text-3xl px-4 text-center">En unos momentos recibira un mensaje via SMS</span>
-            <Link to="/" className="btn btn-accent text-4xl">
+            <Link to="/" className="btn btn-accent text-4xl" reloadDocument>
                 Ir a Inicio
             </Link>
         </div>
